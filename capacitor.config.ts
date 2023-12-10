a6cd82d3f2ae710add3a9fc931e2793f230e4048
LMDevHatem <hatem.nasri@lookiimobile.com>
2023-12-10 19:47:42 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'annonces',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
